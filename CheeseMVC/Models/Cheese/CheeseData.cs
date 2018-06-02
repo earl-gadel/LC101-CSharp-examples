42db26f9220ef7ebcf4746458242fc958920d574
Earl Gadel <earljg@gmail.com>
2018-06-02 13:26:31 -0500

    added cheesedata

---
+++ CheeseMVC/Models/Cheese/CheeseData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Models.Cheese
+{
+    public class CheeseData
+    {
+        private static List<Cheese> cheeses = new List<Cheese>();
+        private static int nextID = 0;
+
+        public static List<Cheese> GetAll()
+        {
+
+        }
+
+        public static Cheese GetById(int cheeseID)
+        {
+
+        }
+
+        public static void Add(Cheese newCheese)
+        {
+
+        }
+
+        public static void Remove(int cheeseID)
+        {
+
+        }
+
+        public static void Update(Cheese cheeseToUpdate)
+        {
+
+        }       
+    }
+}
